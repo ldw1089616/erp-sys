55d19c33abd2d67c5db1cff35745717a15aadfbb
ldw1089616 <dawei.li2@pactera.com>
2018-08-28 17:01:13 +0800

    module add web-api modified

--- web-api/src/main/java/com/mini/Application.java
+++ web-api/src/main/java/com/mini/Application.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -13,7 +16,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableTransactionManagement
 @SpringBootApplication
 @EnableAutoConfiguration
-
+@EnableEurekaClient
 public class Application {
     private static Logger logger = Logger.getLogger(Application.class);
 
